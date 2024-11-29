ece3dd57519f803e36f2be7fc10fd2c5f8749900
amazingOur <786musmanrao@gmail.com>
2024-11-28 21:17:48 -0500

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Use 'react-dom/client' for React 18+
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+// Create the root element for rendering
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the App component
+root.render(<App />);
